31269f9d77f3b8228888fcf256c09ead8eb36638
yazin123 <mohammedyazin4@gmail.com>
2024-11-21 19:34:29 +0530

    admin service ui

--- src/app/admin/dashboard/services/page.js
+++ src/app/admin/dashboard/services/page.js
@@ -254,7 +254,7 @@ const ServiceAdminDashboard = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
         {services.map((service) => (
           <ServiceManagement
             key={service._id}
