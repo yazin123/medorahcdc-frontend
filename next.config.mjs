685e43ed335657daa4859c9270f0d823c5ab6257
Mohammed Yazin <112376598+yazin123@users.noreply.github.com>
2024-12-14 13:14:26 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['localhost','res.cloudinary.com','maps.googleapis.com']
+        domains:['localhost','res.cloudinary.com','maps.googleapis.com', 'medorahcdc.com']
 }
 };
 export default nextConfig;
