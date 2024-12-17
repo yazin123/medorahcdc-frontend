50f156760f86e8b6bc1a56318a2b39e3a906a435
Mohammed Yazin <112376598+yazin123@users.noreply.github.com>
2024-12-17 19:15:07 +0530

    Update Hero.js

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -5,6 +5,14 @@ import Link from 'next/link';
 
 
 const Hero = ({ title, description, image }) => {
+
+     const handleScroll = () => {
+    window.scrollBy({
+      top: 500,  // Scrolls the page 500 pixels down
+      behavior: 'smooth'  // Smooth scroll effect
+    });
+  };
+    
     return (
         <div className={`flex-1 mt-20 ${image?'h-[90vh]':'h-[30vh]'} mb-20`}>
             <div className="md:h-full rounded-t-3xl bg-gradient-to-r from-[#FCD767] to-[#01B0AB]">
@@ -19,7 +27,7 @@ const Hero = ({ title, description, image }) => {
                             </p>
                             {image ? (
                             <div className="space-x-4">
-                                <button className="bg-blue-950 text-white px-6 py-3 rounded-full hover:bg-blue-900" data-aos="fade-right" data-aos-delay="400">
+                                <button className="bg-blue-950 text-white px-6 py-3 rounded-full hover:bg-blue-900" data-aos="fade-right" data-aos-delay="400"  onClick={handleScroll} >
                                     Learn More
                                 </button>
                                 <Link href='/#contact' className="border border-blue-950 text-blue-950 px-6 py-3 rounded-full hover:bg-blue-900 hover:text-white" data-aos="fade-right" data-aos-delay="500">
