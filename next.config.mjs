d01dd092c2e1baa84ff9640c72247d6277e943fb
Mohammed Yazin <112376598+yazin123@users.noreply.github.com>
2024-12-18 11:58:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-       domains:['medorahcdc.com', 'localhost','www.medorahcdc.com']
-}
+    images: {
+        domains: [
+            'medorahcdc.com', 
+            'www.medorahcdc.com', 
+            'localhost'
+        ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.medorahcdc.com',
+                pathname: '/uploads/**'
+            }
+        ]
+    }
 };
+
 export default nextConfig;
