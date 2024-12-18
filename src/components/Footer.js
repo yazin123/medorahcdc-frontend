4d4c04b98cd5be4f7f13649c8fb1f8131cb87074
Mohammed Yazin <112376598+yazin123@users.noreply.github.com>
2024-12-18 11:20:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -59,7 +59,7 @@ const Footer = () => {
 
                         <div className="w-full h-[450px] rounded-lg overflow-hidden shadow-lg">
                             <iframe
-                          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3937.110460900079!2d76.59760667502246!3d9.3234796907501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOcKwMTknMjQuNSJOIDc2wrAzNicwMC43IkU!5e0!3m2!1sen!2sin!4v1731505337359!5m2!1sen!2sin" 
+                          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3937.110922208804!2d76.6001893!3d9.3234388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0623a1b0b15ea9%3A0x89cdd9ccd915bbbc!2sMedorah%20Child%20Development%20Centre!5e0!3m2!1sen!2sin!4v1734500966098!5m2!1sen!2sin" 
                                  width="1000%"
                                 height="100%"
                                 className="border-0"
