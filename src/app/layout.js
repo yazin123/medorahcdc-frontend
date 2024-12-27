87b0d940536b9c0176e6415428a3b1be7f7a4231
Mohammed Yazin <112376598+yazin123@users.noreply.github.com>
2024-12-27 14:32:46 +0530

    tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Poppins } from 'next/font/google';
 import Footer from "@/components/Footer";
 import React from 'react';
-
+import Script from 'next/script';  // Add this import
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -12,6 +12,7 @@ const poppins = Poppins({
   variable: '--font-poppins',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 });
+
 export const metadata = {
   title: 'Medorah CDC',
   description: "Where little minds grow, and big Dreams begin",
@@ -40,9 +41,22 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0WBY0M8XKN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0WBY0M8XKN');
+          `}
+        </Script>
+      </head>
       <ClientComponent />
       <body className={`${poppins.variable} font-poppins`}>
         <Navbar />
