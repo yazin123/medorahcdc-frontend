33aa20bfa06a3a3aafa0479599505c59b1a36459
Mohammed Yazin <112376598+yazin123@users.noreply.github.com>
2025-11-20 04:38:19 +0530

    Refactor image configuration in next.config.mjs
    Updated image configuration to include additional remote patterns and removed specific domains.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,46 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: [
+//             'medorahcdc.com', 
+//             'www.medorahcdc.com', 
+//             'localhost'
+//         ],
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'www.medorahcdc.com',
+//                 pathname: '/uploads/**'
+//             }
+//         ]
+//     }
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-            'medorahcdc.com', 
-            'www.medorahcdc.com', 
-            'localhost'
-        ],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'www.medorahcdc.com',
-                pathname: '/uploads/**'
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "medorahcdc.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.medorahcdc.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
